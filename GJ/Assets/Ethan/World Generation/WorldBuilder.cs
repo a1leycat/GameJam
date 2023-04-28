5a81253d3aa67ec420179acc1876d9059c8fc7bb
BigEshots <88875531+BigEshots@users.noreply.github.com>
2023-04-28 07:04:32 -0700

    It technically works

--- GJ/Assets/Ethan/World Generation/WorldBuilder.cs
+++ GJ/Assets/Ethan/World Generation/WorldBuilder.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour
 {
-    public GameObject[] plotArr;
+    public GameObject Plot;
     GameObject[] list;
     int numPlots;
     GameObject startPlot;
     public GameObject basicPlot;
+    public float plotSpawnTimer;
+    public int chanceForPlot;
     // Start is called before the first frame update
     void Start()
     {
         startPlot = GameObject.FindGameObjectWithTag("start fellow");
+        plotSpawnTimer = Random.Range(1, 5);
+        chanceForPlot = Random.Range(1, 20);
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -54,16 +58,26 @@ public class WorldBuilder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(chanceForPlot == 1)
+        {
+            Destroy(gameObject);
+        }
+        if (plotSpawnTimer > 0)
+        {
+            plotSpawnTimer -= Time.deltaTime;
+        }
+
+
         numPlots = Random.Range(1, 6);
         Debug.Log((startPlot.GetComponent<StartPlotCounter>().numOfPlots).ToString());
-        if (startPlot.GetComponent<StartPlotCounter>().numOfPlots >= 1)
+        if (startPlot.GetComponent<StartPlotCounter>().numOfPlots >= 1 && plotSpawnTimer <= 0)
         {
             Debug.Log("Spawn");
-            Instantiate(plotArr[numPlots], transform.position, transform.rotation);
+            Instantiate(Plot, transform.position, transform.rotation);
             startPlot.GetComponent<StartPlotCounter>().numOfPlots -= 1;
             Destroy(gameObject);
         }
-        if (startPlot.GetComponent<StartPlotCounter>().numOfPlots <= 0)
+        if (startPlot.GetComponent<StartPlotCounter>().numOfPlots <= 0 && plotSpawnTimer <= 0)
         {
             Instantiate(basicPlot, transform.position, transform.rotation);
             Destroy(gameObject);
